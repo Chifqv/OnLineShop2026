7a37ddeb316274c9be634d16d0169010dfd275c1
Элина Кокоева <e.kokoeva06@mail.ru>
2026-05-04 19:13:21 +0300

    userRepositoryInMemory, IUserRepository

---
+++ OnLineShop2026/Data/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Data
+{
+	public interface IUserRepository
+	{
+		void Add(User user);
+		List<User> GetAll();
+		User? TryGetById(Guid userId);
+	}
+}
---
+++ OnLineShop2026/Data/UserRepositoryInMemory.cs
@@ -0,0 +1,19 @@
+﻿using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Data
+{
+	public class UserRepositoryInMemory : IUserRepository
+	{
+		List<User> users = new List<User>() { new User("Элина", "Кокоева", "elina@mail.ru"), new User("Лерочка", "Хубулова", "lera@gmail.com", "/images/userLera.jpg") };
+
+		public void Add(User user)
+		{
+			user.Id = Guid.NewGuid();
+			users.Add(user);
+		}
+
+		public List<User> GetAll() => users;
+
+		public User? TryGetById(Guid userId) => users.FirstOrDefault(user => user.Id == userId);
+	}
+}
